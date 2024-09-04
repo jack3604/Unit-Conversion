0e711b9fd56d48429f54b9f74f2a9cbd648e5e4b
Adisson <akeenan360@gmail.com>
2024-09-04 16:58:57 -0400

    initial argument setup

---
+++ UnitConversion.java
@@ -0,0 +1,14 @@
+public class UnitConversion {
+    public static void main(String[] args) {
+        if (args.length == 3) {
+            System.out.println(args[2] + " " + args[0] + " to " + args[1]);
+        }
+        else {
+            System.out.println("You must use 3 arguments.");
+        }
+
+
+    }
+}
+
+// inches, feet, cubits, steps, yards, miles
