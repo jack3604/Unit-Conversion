ba78d8762f32f12f3e8c909d2a107756aeaa01fb
Adisson <akeenan360@gmail.com>
2024-09-04 17:09:56 -0400

    format arguments to variables

--- UnitConversion.java
+++ UnitConversion.java
@@ -1,7 +1,16 @@
 public class UnitConversion {
+    final double INCHES_TO_FEET = 12; // 12 inches in 1 foot
+
     public static void main(String[] args) {
+        String unitFrom = "";
+        String unitTo = "";
+        int unit = 0;
+
         if (args.length == 3) {
-            System.out.println(args[2] + " " + args[0] + " to " + args[1]);
+            unitFrom = args[0];
+            unitTo = args[1];
+            unit = Integer.parseInt(args[2]);
+            System.out.println(unit + " " + unitFrom + " to " + unitTo);
         }
         else {
             System.out.println("You must use 3 arguments.");
